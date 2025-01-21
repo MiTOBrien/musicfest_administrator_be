80489268d6dc353317bc23ce0a9d45a7cabcf0dc
Michael OBrien <obrien.michael.t@gmail.com>
2025-01-21 13:48:33 -0500

    Added test and logic to delete a show

--- spec/requests/api/v1/shows/shows_request_spec.rb
+++ spec/requests/api/v1/shows/shows_request_spec.rb
@@ -1,1 +1,13 @@
 require "rails_helper"
+
+RSpec.describe "Destory A show" do
+  describe "Destorying A Show" do
+    it 'can destory a show' do
+      show = Show.create!(artist: "Taylor Swift", location: "Stage 1", date: Date.new(2025, 1, 31), time: Time.parse("20:00"))
+      
+      delete "/api/v1/shows/#{show.id}"
+
+      expect(response).to have_http_status(:no_content)
+    end
+  end
+end
