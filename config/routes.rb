7a630a1de88b8ea3486055c4d7e1a538404535dc
Michael OBrien <obrien.michael.t@gmail.com>
2025-01-22 18:46:26 -0500

    Quick push to fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show]
-      resources :schedules, only: [:index, :show, :create, :destoy]
+      resources :schedules, only: [:index, :show, :create, :destroy]
     end
   end
 end
